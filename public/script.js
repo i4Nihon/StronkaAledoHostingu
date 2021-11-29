08227ca461d9daf3df532051747b67f7869423e9
i4Nihon <mjeczmien2007@mail.com>
2021-11-29 23:52:22 +0100

    doing stoper

--- public/script.js
+++ public/script.js
@@ -1,14 +1,55 @@
+const timer = document.getElementById('H1');
 
-function stoper(znacznik) {
-    if (znacznik==1) {
-      start = new Date();
+var hr = 0;
+var min = 0;
+var sec = 0;
+var stoptime = true;
+
+function startTimer() {
+  if (stoptime == true) {
+        stoptime = false;
+        timerCycle();
     }
-    if (znacznik==0 && start>0) {
-      stop = new Date();
-      sekundy = Math.abs(stop-start)/1000;
-      start=0;
-      // alert(sekundy);
-      document.f1.czas.value=sekundy;
-      document.f1.submit();
-    }
+}
+function stopTimer() {
+  if (stoptime == false) {
+    stoptime = true;
   }
+}
+
+function timerCycle() {
+    if (stoptime == false) {
+    sec = parseInt(sec);
+    min = parseInt(min);
+    hr = parseInt(hr);
+
+    sec = sec + 1;
+
+    if (sec == 60) {
+      min = min + 1;
+      sec = 0;
+    }
+    if (min == 60) {
+      hr = hr + 1;
+      min = 0;
+      sec = 0;
+    }
+
+    if (sec < 10 || sec == 0) {
+      sec = '0' + sec;
+    }
+    if (min < 10 || min == 0) {
+      min = '0' + min;
+    }
+    if (hr < 10 || hr == 0) {
+      hr = '0' + hr;
+    }
+
+    timer.innerHTML = hr + ':' + min + ':' + sec;
+
+    setTimeout("timerCycle()", 1000);
+  }
+}
+
+function resetTimer() {
+    timer.innerHTML = '00:00:00';
