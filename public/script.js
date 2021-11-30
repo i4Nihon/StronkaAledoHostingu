12c5ac73378e919415af4fc9039ebac07fa699b2
i4Nihon <mjeczmien2007@gmail.com>
2021-11-30 01:28:27 +0100

    fixing script.js and index

--- public/script.js
+++ public/script.js
@@ -1,9 +1,42 @@
-const timer = document.getElementById('H1');
+const timer = document.getElementById('stopWatch');
 
-var hr = 0;
-var min = 0;
+var msec = 0;
 var sec = 0;
+var min = 0;
 var stoptime = true;
+
+
+
+
+function timerCycle() {
+    if (stoptime == false) {
+    sec = parseInt(sec);
+    min = parseInt(min);
+    
+
+    msec = msec + 1;
+
+    if (msec == 10) {
+      sec = sec + 1;
+      msec = 0;
+    }
+    if (sec == 60) {
+      min = min + 1
+      sec = 0;
+      msec = 0;
+    }
+    if (sec < 10 || sec == 0) {
+      sec = '0' + sec
+    }
+    if (min < 10 || min == 0) {
+      min = '0' + min;
+    }
+
+    timer.innerHTML = min + ':' + sec + ":" + msec;
+
+    setTimeout("timerCycle()", 100);
+  }
+}
 
 function startTimer() {
   if (stoptime == true) {
@@ -11,45 +44,17 @@ function startTimer() {
         timerCycle();
     }
 }
+
 function stopTimer() {
   if (stoptime == false) {
     stoptime = true;
   }
 }
 
-function timerCycle() {
-    if (stoptime == false) {
-    sec = parseInt(sec);
-    min = parseInt(min);
-    hr = parseInt(hr);
-
-    sec = sec + 1;
-
-    if (sec == 60) {
-      min = min + 1;
-      sec = 0;
-    }
-    if (min == 60) {
-      hr = hr + 1;
-      min = 0;
-      sec = 0;
-    }
-
-    if (sec < 10 || sec == 0) {
-      sec = '0' + sec;
-    }
-    if (min < 10 || min == 0) {
-      min = '0' + min;
-    }
-    if (hr < 10 || hr == 0) {
-      hr = '0' + hr;
-    }
-
-    timer.innerHTML = hr + ':' + min + ':' + sec;
-
-    setTimeout("timerCycle()", 1000);
-  }
-}
-
 function resetTimer() {
     timer.innerHTML = '00:00:00';
+    
+    hr=0
+    min=0
+    sec=00
+}
